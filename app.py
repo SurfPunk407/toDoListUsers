e263cb8ea453abf178c095b99dea6fd0d7074e34
Brett Lewis <139373745+SurfPunk407@users.noreply.github.com>
2025-02-18 12:05:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Recommended
-app.config['CORS_HEADERS'] = 'Content-Type'
+#app.config['CORS_HEADERS'] = 'Content-Type'
 
 db.init_app(app)
 
