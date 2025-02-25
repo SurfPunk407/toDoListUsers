e081469909a08a305bac91a6079238158a5c6bd8
Brett Lewis <139373745+SurfPunk407@users.noreply.github.com>
2025-02-25 15:32:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://todolistusers.wuaze.com"])
 app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Recommended
